f36365425bc7f95e6e114d8a9222d49a49b626b3
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-25 13:31:34 +0500

    Import nav components and stylesheet

--- src/screens/navigationBar/NavigationBar.js
+++ src/screens/navigationBar/NavigationBar.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { navigationLinks } from "../../helpers/navigationLinks";
 
 function NavigationBar() {
   return <div>navigation</div>;
