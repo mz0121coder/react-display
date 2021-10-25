ec33ddb5e7810e7843bd61a87dc813828dfd0e10
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-25 14:57:30 +0500

    Navbar functionality

--- src/screens/navigationBar/NavigationBar.js
+++ src/screens/navigationBar/NavigationBar.js
@@ -3,8 +3,25 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { navigationLinks } from "../../helpers/navigationLinks";
 
+function createLinks() {
+  return navigationLinks.map((e, idx) => {
+    <Nav.Link key={idx} href={e.ref}>
+      {e.name}
+    </Nav.Link>;
+  });
+}
+
 function NavigationBar() {
-  return <div>navigation</div>;
+  return (
+    <div id="home">
+      <Navbar expand="md">
+        <Navbar.Brand href="#home">Mohammad Zafar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Nav>{createLinks()}</Nav>
+        <Navbar.Collapse></Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default NavigationBar;
