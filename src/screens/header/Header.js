cdc680f8114a1c20dc43a3b95ccbf79b8195fda3
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-26 16:26:47 +0500

    Create transition effect

--- src/screens/header/Header.js
+++ src/screens/header/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "./header.css";
 import Typed from "react-typed";
 
 function Header() {
   return (
-    <div>
+    <div className="main-info">
       <h1>Welcome to my Portfolio</h1>
+      <Typed
+        strings={[" ", "Frontend Developer", "Trader", "Coder", " ", " "]}
+        typeSpeed={40}
+        backSpeed={50}
+        loop
+      >
+        <input type="text" />
+      </Typed>
     </div>
   );
 }
