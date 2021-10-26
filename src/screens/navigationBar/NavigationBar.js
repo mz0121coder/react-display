14ca8dfa0a58f651013f4dad8f6a7dab916d171b
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-26 15:07:38 +0500

    text formatting(1)

--- src/screens/navigationBar/NavigationBar.js
+++ src/screens/navigationBar/NavigationBar.js
@@ -1,24 +1,46 @@
 import React from "react";
+import "./navigation.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { navigationLinks } from "../../helpers/navigationLinks";
 
 function createLinks() {
-  return navigationLinks.map((e, idx) => {
+  return navigationLinks.map((e, idx) => (
     <Nav.Link key={idx} href={e.ref}>
       {e.name}
-    </Nav.Link>;
-  });
+    </Nav.Link>
+  ));
 }
 
 function NavigationBar() {
   return (
     <div id="home">
-      <Navbar expand="md">
-        <Navbar.Brand href="#home">Mohammad Zafar</Navbar.Brand>
+      <Navbar
+        className="navigation__container"
+        style={{
+          zIndex: "2",
+          position: "fixed",
+          top: "0",
+          width: "100%",
+        }}
+        collapseOnSelect
+        expand="md"
+      >
+        <Navbar.Brand style={{ marginLeft: "1rem" }} href="#home">
+          Mohammad Zafar
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Nav>{createLinks()}</Nav>
-        <Navbar.Collapse></Navbar.Collapse>
+        <Navbar.Collapse
+          style={{
+            justifyContent: "flex-end",
+            marginRight: "1rem",
+            borderColor: "none",
+          }}
+        >
+          <Nav className="links" style={{ margin: "0 1rem" }}>
+            {createLinks()}
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
