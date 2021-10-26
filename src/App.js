1415eea72fafdbee516fd3ab1409c984f1f7bc5c
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-26 15:32:10 +0500

    Install + style particle animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Navigation from "./screens/navigationBar/NavigationBar";
+import ParticleAnimation from "react-particle-animation";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <ParticleAnimation
+        style={{ height: "96vh" }}
+        background={{ r: 21, g: 22, b: 23, a: 1 }}
+        particleSpeed={0.1}
+        particleRadius={1.5}
+      />
     </div>
   );
 }
