a4a71b2a706e731e52049afd5a6e5a9c7d5536fb
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-26 17:04:07 +0500

    'About' section styling

--- src/screens/aboutMe/AboutMe.js
+++ src/screens/aboutMe/AboutMe.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./aboutMe.css";
+import person from "../../images/Coding.png";
 
 function AboutMe() {
-  return <div>About Me</div>;
+  return (
+    <div className="about__container" id="about-me">
+      <div>
+        <img src={person} alt="person icon" />
+      </div>
+      <div className="about__text">
+        <h1>ABOUT ME</h1>
+        <p>
+          I started my coding coding journey in mid 2021 via a full stack
+          bootcamp at University of Birmingham. Problem solving and creative
+          projects are my biggest interests in this field.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
