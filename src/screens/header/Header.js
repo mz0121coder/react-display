6692235d9c98823874518821c1c8863500cc610a
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-26 15:54:06 +0500

    Install react-typed, create + import header files

---
+++ src/screens/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Typed from "react-typed";
+
+function Header() {
+  return (
+    <div>
+      <h1>Welcome to my Portfolio</h1>
+    </div>
+  );
+}
+
+export default Header;
