69d116f36c23c6f397a0e3b9bd18ff00ff627809
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-26 17:05:55 +0500

    Export tech icons

---
+++ src/helpers/techs.js
@@ -0,0 +1,38 @@
+export const techs = [
+  {
+    name: "React",
+    iconName: "react",
+  },
+  {
+    name: "NodeJS",
+    iconName: "nodejs",
+  },
+  {
+    name: "Javascript",
+    iconName: "javascript",
+  },
+  {
+    name: "CSS",
+    iconName: "css3",
+  },
+  {
+    name: "HTML",
+    iconName: "html5",
+  },
+  {
+    name: "Mongo DB",
+    iconName: "mongodb",
+  },
+  {
+    name: "Bootstrap",
+    iconName: "bootstrap",
+  },
+  {
+    name: "Terminal",
+    iconName: "terminal",
+  },
+  {
+    name: "Github",
+    iconName: "github",
+  },
+];
