23db3004da254855324c57163e665ecdb4a61390
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-27 16:51:30 +0500

    Add projects data

--- src/helpers/projects.js
+++ src/helpers/projects.js
@@ -9,4 +9,69 @@ import createPassword from "../images/create-a-password.png";
 import planner from "../images/plan-your-day.png";
 import quiz from "../images/quiz.png";
 
+const projects = [
+  {
+    image: trainingJournal,
+    github: "https://github.com/mz0121coder/Training-Journal",
+    live: "https://training-journal-mz.herokuapp.com/",
+    desc: "Training Journal",
+    summary: "Workout tracker",
+    tech: "MongoDB | Mongoose | Express",
+  },
+  {
+    image: engineerPathways,
+    github: "https://github.com/mz0121coder/Engineer-Pathways",
+    live: "https://engineerpathways.herokuapp.com/",
+    desc: "Engineer Pathways",
+    summary: "Full stack team manager application",
+    tech: "Node.js | Handlebars.js | MySQL",
+  },
+  {
+    image: ecomServer,
+    github: "https://github.com/mz0121coder/Ecommerce-server",
+    desc: "Ecommerce Server",
+    summary: "Building the back-end for an ecommerce site",
+    tech: "Node.js | Express | Insomnia",
+  },
+  {
+    image: takeNotes,
+    github: "https://github.com/mz0121coder/Take-notes",
+    live: "https://take-notes-online.herokuapp.com/",
+    desc: "Take Notes",
+    summary: "Note taking app",
+    tech: "Node.js | Express | JavaScript",
+  },
+  {
+    image: checkWeather,
+    github: "https://github.com/mz0121coder/Check-the-weather",
+    live: "https://mz0121coder.github.io/Check-the-weather/",
+    desc: "Check the Weather",
+    summary: "Current conditions & 5 day forecast, using Open Weather API",
+    tech: "CSS | API | Frontend JS",
+  },
+  {
+    image: createPassword,
+    github: "https://github.com/mz0121coder/Create-a-Password",
+    live: "https://mz0121coder.github.io/Create-a-Password/",
+    desc: "Create a Password",
+    summary: "Using JavaScript to generate unique passwords",
+    tech: "Vanilla JS",
+  },
+  {
+    image: planner,
+    github: "https://github.com/mz0121coder/Plan-your-day",
+    live: "https://mz0121coder.github.io/Plan-your-day/",
+    desc: "Plan your day",
+    summary: "Colour coded planner for 9-5 activities",
+    tech: "JavaScript | Local Storage",
+  },
+  {
+    image: quiz,
+    github: "https://github.com/mz0121coder/JS-multi-choice-quiz",
+    live: "https://mz0121coder.github.io/JS-multi-choice-quiz",
+    desc: "JS Quiz",
+    summary: "A challenging, multi-choice tech quiz",
+    tech: "HTML | CSS | JavaScript",
+  },
+];
 export default projects;
