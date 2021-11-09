47dbfa8b52e4d669f3c096ab5e896aafa633c600
Mohammad Zafar <mzcoder7@gmail.com>
2021-11-10 03:17:27 +0500

    Rename folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Navigation from "./screens/navigationBar/NavigationBar";
+import Navigation from "./components/navigationBar/NavigationBar";
 import ParticleAnimation from "react-particle-animation";
-import Header from "./screens/header/Header";
-import AboutMe from "./screens/aboutMe/AboutMe";
-import Technologies from "./screens/technologies/Technologies";
-import Portfolio from "./screens/portfolio/Portfolio";
-import ContactMe from "./screens/contactMe/ContactMe";
+import Header from "./components/header/Header";
+import AboutMe from "./components/aboutMe/AboutMe";
+import Technologies from "./components/technologies/Technologies";
+import Portfolio from "./components/portfolio/Portfolio";
+import ContactMe from "./components/contactMe/ContactMe";
 
 function App() {
   return (
